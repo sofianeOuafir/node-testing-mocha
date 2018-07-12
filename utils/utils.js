8b77e3d3581313084ab7a8b2a00a9350030a112c
Sofiane OUAFIR <sofiane.ouafir@live.fr>
2018-07-12 18:05:37 +0100

    learnt about async testing

--- utils/utils.js
+++ utils/utils.js
@@ -1,8 +1,34 @@
-module.exports.add = (a, b) => a + b;
-module.exports.square = (x) => x * x;
-module.exports.setName = (user, fullname) => {
+function add(a, b){
+  return a + b;
+}
+
+function asyncAdd(a, b, callback) {
+  setTimeout(() => {
+    callback(a + b);
+  }, 1000);
+};
+
+function square(x){
+  return x * x;
+}
+
+function asyncSquare(x, callback) {
+  setTimeout(() => {
+    callback(x * x);
+  }, 1000); 
+}
+
+function setName(user, fullname){
   var names = fullname.split(" ");
   var [firstname, lastname] = names;
   user.firstname = firstname;
   user.lastname = lastname;
-};
+}
+
+module.exports = {
+  add, 
+  square,
+  setName,
+  asyncAdd,
+  asyncSquare
+}
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -8,11 +8,25 @@ it('should add 2 numbers', () => {
     .toBeA('number');
 });
 
+it('should add to number asynchronously', (done) => {
+  var res = utils.asyncAdd(10, 2, (sum) => {
+    expect(sum).toBe(12).toBeA('number');
+    done();
+  });
+});
+
 it('should square a number', () => {
   var res = utils.square(9);
   expect(res)
     .toBe(81)
     .toBeA('number');
+});
+
+it('should square a number asynchronously', (done) => {
+  utils.asyncSquare(10, (result) => {
+    expect(result).toBe(100).toBeA('number');
+    done();
+  })
 });
 
 it('should set the names correctly', () => {
@@ -27,3 +41,4 @@ it('should set the names correctly', () => {
       lastname: 'OUAFIR'
     });
 });
+
